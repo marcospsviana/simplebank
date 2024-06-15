7821b0fc1612ad412eabd4364c1261ea8fc5d86e
marcospsviana <marcospaulo.silvaviana@gmail.com>
2024-06-15 23:35:59 +0100

    add ci, flake8 and black

--- bank.py
+++ bank.py
@@ -1,5 +1,3 @@
-from datetime import datetime as dt
-
 from formaters import currency_formater, date_formater
 
 menu = """
@@ -12,16 +10,17 @@ menu = """
 
 ***********************************************
 """
-extract = "**************************** ACCOUNT EXTRACT ****************************\n\n\n"
+extract = (
+    "**************************** ACCOUNT EXTRACT ****************************\n\n\n"
+)
 value = 0
 
 
 OPERATIONS_REPORT = {
     "deposit": "Deposit ",
     "withdrawal": "Withdrawal ",
-    "account_balance": "Account Balance"
+    "account_balance": "Account Balance",
 }
-
 
 
 def record_operations(type_operation, val, time):
@@ -32,49 +31,60 @@ def record_operations(type_operation, val, time):
 LIMIT = 500
 account_balance = 0
 WITHDRAWAL_COUNT = 0
-option_choice = ''
+option_choice = ""
 print(menu)
-record_operations("account_balance", currency_formater(account_balance), date_formater())
+record_operations(
+    "account_balance", currency_formater(account_balance), date_formater()
+)
 
 
 while True:
     option_choice = input("Choose an option from the menu\n").lower()
 
-    if option_choice == 'd':
+    if option_choice == "d":
         value = float(input("Enter with value of the deposit\n"))
         if value > 0:
             account_balance += value
             extract += "  ---------------------------------------------  \n"
             record_operations("deposit", currency_formater(value), date_formater())
-            record_operations("account_balance", currency_formater(account_balance), date_formater())
+            record_operations(
+                "account_balance", currency_formater(account_balance), date_formater()
+            )
 
         else:
             print("Invalid value inputted\n")
 
     elif option_choice == "w":
         if WITHDRAWAL_COUNT == 3:
-            print("""************************ ATTENTION ************************
+            print(
+                """************************ ATTENTION ************************
                       You exceed the daily limit withdrawal,
                        you can do 3 withdrawal per day!')
-                      ***********************************************************""")
+                      ***********************************************************"""
+            )
         value = float(input("Enter with value of the withdrawal\n"))
         if value > 500:
             print("You exceed the maxim limit of R$ 500,00")
         if value > account_balance:
-            print(f"""
-                The value of {currency_formater(value)} exceed the limit of your balance account, 
+            print(
+                f"""
+                The value of {currency_formater(value)} exceed the limit of your balance account,
                 you have {currency_formater(account_balance)} of balance!"""
-                  )
+            )
         else:
             account_balance -= value
             extract += "  ---------------------------------------------  \n"
             record_operations("withdrawal", currency_formater(value), date_formater())
-            record_operations("account_balance", currency_formater(account_balance), date_formater())
+            record_operations(
+                "account_balance", currency_formater(account_balance), date_formater()
+            )
             WITHDRAWAL_COUNT += 1
 
     elif option_choice == "e":
         print(extract)
-        print("****************************** END EXTRACT ******************************")
+        print(
+            "****************************** END EXTRACT ******************************"
+        )
 
     elif option_choice == "q":
         break
--- setup_database.py
+++ setup_database.py
@@ -3,16 +3,15 @@ import sqlite3
 database_con = sqlite3.connect("bank.db")
 cursor = database_con.cursor()
 
+
 def setup_db_tables():
     sql_statements = [
-        
-    """CREATE TABLE IF NOT EXISTS user(
+        """CREATE TABLE IF NOT EXISTS user(
                    id INTEGER PRIMARY KEY,
                    name TEXT NOT NULL,
                    idnumber TEXT NOT NULL
                    );""",
-    
-    """CREATE TABLE IF NOT EXISTS address( 
+        """CREATE TABLE IF NOT EXISTS address(
                    id INTEGER PRIMARY KEY NOT NULL,
                    user_id INTEGER NOT NULL,
                    name TEXT NOT NULL,
@@ -23,25 +22,24 @@ def setup_db_tables():
                    city TEXT NOT NULL,
                    FOREIGN KEY(user_id) references user(id)
                    );""",
-    
-    """CREATE TABLE IF NOT EXISTS account(
+        """CREATE TABLE IF NOT EXISTS account(
                    id INTEGER PRIMARY KEY,
                    user_id INTEGER NOT NULL,
                    name TEXT NOT NULL,
                    account_number TEXT NOT NULL,
                    FOREIGN KEY(user_id) references user(id)
-                   );"""
+                   );""",
     ]
     try:
         with sqlite3.connect("bank.db") as conn:
             cursor = conn.cursor()
             for statement in sql_statements:
                 cursor.execute(statement)
-            
+
             conn.commit()
     except sqlite3.Error as e:
         print(e)
-   
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     setup_db_tables()
